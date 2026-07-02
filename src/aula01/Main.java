a08f78d9f193632ca4f9adacfd6070c7cb289ff0
Lucas Vinícius da Silva <lucasv2224@outlook.com>
2026-07-02 16:25:11 -0300

    variaveis e tipos primitivos

---
+++ src/aula01/Main.java
@@ -0,0 +1,14 @@
+package aula01;
+
+public class Main{
+    public static void main(String[] args) {
+        int idade = 18;
+        double salarioMensal = 1500.00;
+        char sexo = 'M';
+        boolean empregado = false;
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário Mensal: " + salarioMensal);
+        System.out.println("Sexo: " + sexo);
+    }
+}
