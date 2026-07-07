022ba13e3cd5effbbdf3ae97e284855f591b5dce
Lucas Vinícius da Silva <lucasv2224@outlook.com>
2026-07-07 17:06:50 -0300

    operadores comparativos

---
+++ src/aula05/OperadoresComparativos.java
@@ -0,0 +1,15 @@
+package aula05;
+
+public class OperadoresComparativos {
+    public static void main(String[] args) {
+        int pao = 10;
+        int acucar = 15;
+
+        System.out.println(pao == acucar); // false
+        System.out.println(pao != acucar); // true
+        System.out.println(pao > acucar); // false
+        System.out.println(pao < acucar); // true
+        System.out.println(pao >= acucar); // false
+        System.out.println(pao <= acucar); // true
+    }
+}
