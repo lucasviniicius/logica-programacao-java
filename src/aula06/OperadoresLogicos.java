53f49284e595e273530f8e12eea4c18c85255dda
Lucas Vinícius da Silva <lucasv2224@outlook.com>
2026-07-07 17:19:33 -0300

    operadores lógicos

---
+++ src/aula06/OperadoresLogicos.java
@@ -0,0 +1,13 @@
+package aula06;
+
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println(x && y); // false
+        System.out.println(x || y); // true
+        System.out.println(!x); // false
+        System.out.println(!y); // true
+    }
+}
